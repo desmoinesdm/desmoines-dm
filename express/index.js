c2e579a1cc588ddde940e2128114ece00d5c9270
pagecow <pagecow@gmail.com>
2020-11-22 05:57:58 -0600

    first commit

---
+++ express/index.js
@@ -0,0 +1,21 @@
+window.onload = function() { 
+    // header bottom border function
+    window.onscroll = function() { addHeaderBorder() };
+    var header = document.getElementById("header");
+    function addHeaderBorder() {
+        if (window.pageYOffset > 50) {
+            header.classList.add("header-box-shadow");
+        } else {
+            header.classList.remove("header-box-shadow");
+        }
+    }
+
+    // $('.navbar-nav>li>a').on('click', function(){
+    //     $('.navbar-collapse').collapse('hide');
+    // });
+
+    // var shiftWindow = function() { scrollBy(0, -50) };
+
+    // pop up description boxes
+    // on hover, turn off display: none of box, and turn back on once no longer hover
+}
