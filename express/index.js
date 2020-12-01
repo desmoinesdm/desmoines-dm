37a0ab2e2d1ef7f29c6ccb3ce455e0a552a68e59
pagecow <pagecow@gmail.com>
2020-12-01 07:35:13 -0600

    update

--- express/index.js
+++ express/index.js
@@ -1,8 +1,9 @@
 window.onload = function() { 
-    // header bottom border function
     window.onscroll = function() { addHeaderBorder() };
+
     var header = document.getElementById("header");
     var adj = document.querySelectorAll(".adj");
+
     function addHeaderBorder() {
         if (window.pageYOffset > 300) {
             header.classList.add("header-box-shadow");
@@ -11,14 +12,12 @@ window.onload = function() {
             header.classList.remove("header-box-shadow");
             adj.forEach(e => e.classList.remove("adjust"));
         }
+
+        addFadeInLeft();
     }
 
     $('.navbar-nav>li>a').on('click', function(){
         $('.navbar-collapse').collapse('hide');
     });
 
-    // var shiftWindow = function() { scrollBy(0, -50) };
-
-    // pop up description boxes
-    // on hover, turn off display: none of box, and turn back on once no longer hover
 }
