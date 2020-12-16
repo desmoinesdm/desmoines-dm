0ecf2ae2a992bbb47e9a5a00039d758c0579c7a3
pagecow <pagecow@gmail.com>
2020-12-16 07:42:52 -0600

    updated

--- express/index.js
+++ express/index.js
@@ -5,6 +5,7 @@ window.onload = function() {
     var headerButton = document.getElementById("contact-button");
     var headerImg = document.getElementById("header-img");
     var headerImgTwo = document.getElementById("header-img-2");
+    var sandwich = document.getElementById("sandwich");
 
     var adj = document.querySelectorAll(".adj");
 
@@ -21,10 +22,14 @@ window.onload = function() {
             headerButton.classList.add("btn-primary-2");
             headerImg.classList.add("display-none");
             headerImgTwo.classList.remove("display-none");
+            sandwich.classList.add("fourth-color");
+            sandwich.classList.remove("sec-color");
         } else {
             headerButton.classList.remove("btn-primary-2");
             headerImg.classList.remove("display-none");
             headerImgTwo.classList.add("display-none");
+            sandwich.classList.remove("fourth-color");
+            sandwich.classList.add("sec-color");
         }
     }
 
