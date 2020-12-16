0004a98839d7d905450c4b7e2fd4d4961b38e06e
pagecow <pagecow@gmail.com>
2020-12-16 07:28:16 -0600

    updated

--- express/index.js
+++ express/index.js
@@ -1,7 +1,11 @@
 window.onload = function() { 
     window.onscroll = function() { addHeaderBorder() };
-
     var header = document.getElementById("header");
+    var header2Offset = $("#fast").offset().top - 250;
+    var headerButton = document.getElementById("contact-button");
+    var headerImg = document.getElementById("header-img");
+    var headerImgTwo = document.getElementById("header-img-2");
+
     var adj = document.querySelectorAll(".adj");
 
     function addHeaderBorder() {
@@ -13,7 +17,15 @@ window.onload = function() {
             adj.forEach(e => e.classList.remove("adjust"));
         }
 
-        addFadeInLeft();
+        if(window.pageYOffset > header2Offset) {
+            headerButton.classList.add("btn-primary-2");
+            headerImg.classList.add("display-none");
+            headerImgTwo.classList.remove("display-none");
+        } else {
+            headerButton.classList.remove("btn-primary-2");
+            headerImg.classList.remove("display-none");
+            headerImgTwo.classList.add("display-none");
+        }
     }
 
     $('.navbar-nav>li>a').on('click', function(){
