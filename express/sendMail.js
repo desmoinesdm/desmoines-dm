383f05044902a8ca99eb2b8c53f90392a7bbd8fb
pagecow <pagecow@gmail.com>
2020-12-20 09:13:31 -0600

    updated

--- express/sendMail.js
+++ express/sendMail.js
@@ -8,8 +8,21 @@ function sendMail(e) {
     let phone = document.getElementById("phone").value;
     let subject = document.getElementById("subject").value;
 
-    axios
-        .post("http://localhost:4040/api/contact", {name: name, email: email, phone: phone, subject: subject})
-        .then(res => console.log("res: ", res))
-        .catch(err => console.log("err: ", err))
+    console.log("sendMail fired!", name, email, phone, subject);
+
+    fetch('http://localhost:4040/api/contact', {
+        method: 'POST', 
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name: name, email: email, phone: phone, subject: subject}),
+      })
+    //   .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
 }
