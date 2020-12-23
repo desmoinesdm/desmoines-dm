33104ed485684a47a73cfd147534bb9def02f29c
pagecow <pagecow@gmail.com>
2020-12-23 06:31:36 -0600

    update

--- express/sendMail.js
+++ express/sendMail.js
@@ -8,6 +8,10 @@ function sendMail(e) {
     let phone = document.getElementById("phone").value;
     let subject = document.getElementById("subject").value;
 
+    let contactH1 = document.getElementById("contact-h1");
+    let form = document.getElementById("sendMail");
+    let thankYou = document.getElementById("thank-you");
+
     console.log("sendMail fired!", name, email, phone, subject);
 
     fetch('/api/contact', {
@@ -21,6 +25,9 @@ function sendMail(e) {
     //   .then(response => response.json())
       .then(data => {
         console.log('Success:', data);
+        contactH1.classList.add("display-none");
+        form.classList.add("display-none");
+        thankYou.classList.remove("display-none");
       })
       .catch((error) => {
         console.error('Error:', error);
